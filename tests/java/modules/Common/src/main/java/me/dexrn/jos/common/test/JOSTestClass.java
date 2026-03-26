646e38c2419b4379da6d014ab72896d7eae82bdf
Dexrn ZacAttack <me@dexrn.me>
2026-03-26 00:18:53 -0700

    feat: super classes are included in classData parsing, now it properly reads a Minecraft Classic 0.30 world.

--- tests/java/modules/Common/src/main/java/me/dexrn/jos/common/test/JOSTestClass.java
+++ tests/java/modules/Common/src/main/java/me/dexrn/jos/common/test/JOSTestClass.java
@@ -10,39 +10,43 @@
  */
 package me.dexrn.jos.common.test;
 
+import me.dexrn.jos.common.test.polymorphism.JOSPolymorphicClass;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 public class JOSTestClass implements Serializable {
-	public class Subclass implements Serializable {
-		boolean b = true;
-		char c = 'b';
-		byte bt = 7;
-		short s = 8;
-		int i = 9;
-		long l = 10;
-		float f = 11.0f;
-		double d = 12.0;
-	}
+//	public class Subclass implements Serializable {
+//		boolean b = true;
+//		char c = 'b';
+//		byte bt = 7;
+//		short s = 8;
+//		int i = 9;
+//		long l = 10;
+//		float f = 11.0f;
+//		double d = 12.0;
+//	}
 	
-	Subclass sc = new Subclass();
-	JOSDataClass dc = new JOSDataClass();
-	JOSArrayClass ac = new JOSArrayClass();
-	JOSNestedArrayClass nac = new JOSNestedArrayClass();
-
-	boolean b = false;
-	char c = 'a';
-	byte bt = 1;
-	short s = 2;
-	int i = 3;
-	long l = 4;
-	float f = 5.0f;
-	double d = 6.0;
-	String str = "Hello, world!";
-
-	List<Character> carr = new ArrayList<Character>();
-	{
-		str.chars().forEach(c -> carr.add((char) c));
-	}
+//	Subclass sc = new Subclass();
+//	JOSDataClass dc = new JOSDataClass();
+//	JOSArrayClass ac = new JOSArrayClass();
+//	JOSNestedArrayClass nac = new JOSNestedArrayClass();
+//	JOSEmptyClass ec = new JOSEmptyClass();
+	JOSPolymorphicClass pc = new JOSPolymorphicClass();
+	
+//	boolean b = false;
+//	char c = 'a';
+//	byte bt = 1;
+//	short s = 2;
+//	int i = 3;
+//	long l = 4;
+//	float f = 5.0f;
+//	double d = 6.0;
+//	String str = "Hello, world!";
+//
+//	List<Character> carr = new ArrayList<Character>();
+//	{
+//		str.chars().forEach(c -> carr.add((char) c));
+//	}
 }
